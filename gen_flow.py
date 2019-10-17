Fixed scaling of sampling operation

gen_flow.py
@@ -15,7 +15,7 @@ from helper import int_shape, split_along_channels, GaussianIsotrop, LogisticDis
 from glow import Encoder, GlowNet
 
 
-NAME = 's05'
+NAME = 's13'
 
 class GenerativeFlow(tf.keras.Model):
     def __init__(self, input_shape, encoder, use_gauss=True, name='genflow', ml=True, **kwargs):
@@ -30,7 +30,8 @@ class GenerativeFlow(tf.keras.Model):
         if use_gauss:
             self.dist = GaussianIsotrop(zeros, zeros)
         else:
-            self.dist = LogisticDist(zeros, zeros)
+            ones = tf.ones(shape=self.out_shape)
+            self.dist = LogisticDist(zeros, ones)
         
     def call(self, inputs):
         for layer in self.encoder:
@@ -47,8 +48,8 @@ class GenerativeFlow(tf.keras.Model):
         z = tf.reshape(z, shape=[1] + list(self.out_shape))
         # forward pass to fill the losses with logdet values
         self.call(self.invert(z))
-        logdet = sum(self.losses)
-        z /= logdet
+        logdet = sum(tf.math.exp(self.losses))
+        #z /= logdet
         x = self.invert(z)
         return x
         
@@ -61,7 +62,7 @@ class GenerativeFlow(tf.keras.Model):
                 encoder.append(perm_layer)       
             else:
                 encoder.append(Conv1x1(ml=True))
-            encoder.append(CouplingLayer(ml=True))
+            encoder.append(CouplingLayer(ml=True, filters=(32,32)))
         return cls(input_shape, encoder, use_gauss)
         
     @classmethod
@@ -147,6 +148,18 @@ def re_shape(tensor):
         y_vals.append(tensor[i, 0, 0, 1])
     return x_vals, y_vals
     
+def plot_dist_samples(dist):
+    points = [dist.sample() for i in range(batch_size)]
+    x_vals = []
+    y_vals = []
+    for p in points:
+        x_vals.append(p[ 0, 0, 0])
+        y_vals.append(p[ 0, 0, 1])
+    fig = plt.figure()
+    plt.plot(x_vals, y_vals, 'ro')
+    plt.show()
+    fig.savefig('dist' + '_generate.png')
+    
     
 def test_sampling(model):
     images = [model.sample() for i in range(4)]
@@ -175,6 +188,7 @@ def show_space_contraction(model, all_layers=False):
     horizontal_lines = []
     vertical_lines = []
     line = np.linspace(-2.0, 2.0, 256)
+    #line = np.linspace(-16.0, 16.0, 256)
     line = tf.cast(tf.reshape(line, shape=(256,1,1,1)), 'float32')
     for i in range(3):
         y_plus = tf.zeros(shape=line.get_shape(), dtype=tf.dtypes.float32) + float(i)
@@ -220,12 +234,18 @@ def show_space_contraction(model, all_layers=False):
             axes[1].plot(x, y, 'ro--', linewidth=2, markersize=3)  
         
     plt.show()
-    fig.savefig(NAME + '_contract_all_back.png')
+    if all_layers:
+        name_add = '_all'
+    else:
+        name_add = ''
+    
+    fig.savefig(NAME + name_add + '_contract_back.png')
     
 def run():
     dataset = [generate_data(batch_size) for i in range(100)]
-    model = GenerativeFlow.buildSimpleNet(input_shape=(1, 1, 2), blocks=10, use_permutations=True)
+    model = GenerativeFlow.buildSimpleNet(input_shape=(1, 1, 2), use_gauss=False, blocks=3, use_permutations=False)
     training(model, dataset, 20)
     sample_points(model)
     show_space_contraction(model)
+    show_space_contraction(model, True)
     

helper.py
@@ -57,25 +57,23 @@ class LogisticDist(object):
     """A discretized logistic distribution that can be used for sampling and 
         calculating the log density.
     """
-    def __init__(self, mean, log_scale):
+    def __init__(self, mean, scale):
         self.mean = mean
-        self.log_scale = log_scale
+        self.scale = scale
         
     def logp(self, x):
-        #x = (x - self.mean) / (2.0 * tf.math.exp(self.log_scale + 1e-10))
-        #logp_val = - self.log_scale - tf.math.log(tf.math.exp(x) + tf.math.exp(-x) + 1e-10)
-        logp_val = - tf.math.log(1 + tf.math.exp(x)) - tf.math.log(1 + tf.math.exp(-x))        
+        x = (x - self.mean) / (2.0 * self.scale)
+        logp_val = - tf.math.log( self.scale + 1e-10) - tf.math.log(tf.math.exp(x) + tf.math.exp(-x) + 1e-10)
+        #logp_val = - tf.math.log(1 + tf.math.exp(x + 1e-10)) - tf.math.log(1 + tf.math.exp(-x + 1e-10))        
         return tf.reduce_sum(logp_val, axis=[1,2,3])
         
     def eps_recon(self, x):
-        return (x - self.mean) / tf.exp(self.log_scale)
+        return (x - self.mean) / self.scale
     
     def sample(self, eps=None):
         if eps is None:
             eps_unif = tf.keras.backend.random_uniform(shape=self.mean.get_shape())
             eps = tf.math.log(eps_unif) - tf.math.log(1. - eps_unif)
-        return self.mean + tf.exp(self.log_scale) * eps            
+        return self.mean + self.scale * eps            
 
-    
-    
-   
+